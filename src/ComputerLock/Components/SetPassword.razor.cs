85479ad15c1fbc149a78780150f6566d02480f77
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2024-10-16 20:51:45 +0800

    第一次启动时通过引导页面设置密码

---
+++ src/ComputerLock/Components/SetPassword.razor.cs
@@ -0,0 +1,51 @@
+﻿namespace ComputerLock.Components;
+public partial class SetPassword
+{
+    [Inject]
+    private IStringLocalizer<Lang> Lang { get; set; } = default!;
+
+    [Inject]
+    private ISnackbar Snackbar { get; set; } = default!;
+
+
+    private bool _isShow;
+    private string _password = "";
+    private InputType _passwordInputType = InputType.Password;
+    private string _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+    [Parameter]
+    public EventCallback<string> PasswordSetFinished { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+    }
+
+    private void ShowPassword()
+    {
+        if (_isShow)
+        {
+            _isShow = false;
+            _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+            _passwordInputType = InputType.Password;
+        }
+        else
+        {
+            _isShow = true;
+            _passwordInputIcon = Icons.Material.Filled.Visibility;
+            _passwordInputType = InputType.Text;
+        }
+    }
+
+    private async Task SavePasswordAsync()
+    {
+        if (_password.IsEmpty())
+        {
+            Snackbar.Add(Lang["PasswordEmpty"], Severity.Error);
+            return;
+        }
+
+        var password = JiuLing.CommonLibs.Security.MD5Utils.GetStringValueToLower(_password);
+        await PasswordSetFinished.InvokeAsync(password);
+    }
+}

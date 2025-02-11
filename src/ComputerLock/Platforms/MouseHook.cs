0b78e01948a9468826a3e5c9ee3bf743b39f1934
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2025-02-11 23:46:14 +0800

    重构自动移动鼠标逻辑

--- src/ComputerLock/Platforms/MouseHook.cs
+++ src/ComputerLock/Platforms/MouseHook.cs
@@ -6,7 +6,19 @@ internal class MouseHook
     [DllImport("user32.dll")]
     private static extern int ShowCursor(bool bShow);
 
+    [DllImport("user32.dll")]
+    private static extern int SetCursorPos(int x, int y);
+
+    [DllImport("user32.dll")]
+    private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+    public const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+    public const int MOUSEEVENTF_LEFTUP = 0x0004;
+    public const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+    public const int MOUSEEVENTF_RIGHTUP = 0x0010;
+
     private int _cursorCount = 0;
+    private Random _random = new Random();
 
     /// <summary>
     /// 隐藏鼠标光标。
@@ -46,5 +58,18 @@ internal class MouseHook
             ShowCursor(false); // 隐藏光标
             _cursorCount--;
         }
+    }
+
+    /// <summary>
+    /// 移动鼠标并点击
+    /// </summary>
+    public void MoveAndClick()
+    {
+        var x = _random.Next(0, 100);
+        var y = _random.Next(0, 100);
+
+        var p = new Point(x, y);
+        SetCursorPos((int)p.X, (int)p.Y);
+        mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, x, y, 0, 0);
     }
 }

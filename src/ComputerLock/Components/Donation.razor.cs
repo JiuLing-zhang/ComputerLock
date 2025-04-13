855e4e0e2953b625df3717f98a760a49d14ee9cd
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2025-04-13 23:09:44 +0800

    赞赏页面样例

---
+++ src/ComputerLock/Components/Donation.razor.cs
@@ -0,0 +1,8 @@
+﻿namespace ComputerLock.Components;
+
+public partial class Donation
+{
+    [CascadingParameter]
+    private IMudDialogInstance MudDialog { get; set; } = null!;
+    private void Close() => MudDialog.Cancel();
+}

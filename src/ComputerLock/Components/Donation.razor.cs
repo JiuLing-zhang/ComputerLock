a59d7af014a90fa354d403edcfada8205f4e07b9
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2025-04-20 09:06:42 +0800

    打赏人数

--- src/ComputerLock/Components/Donation.razor.cs
+++ src/ComputerLock/Components/Donation.razor.cs
@@ -1,8 +1,25 @@
-﻿namespace ComputerLock.Components;
+﻿
+using System.Net.Http;
+using System.Text.Json;
+
+namespace ComputerLock.Components;
 
 public partial class Donation
 {
     [CascadingParameter]
     private IMudDialogInstance MudDialog { get; set; } = null!;
     private void Close() => MudDialog.Cancel();
+
+    private int? _donationCount;
+    private static readonly HttpClient HttpClient = new HttpClient();
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        var json = await HttpClient.GetStringAsync("https://www.jiuling.cc/api/donation/list");
+        using JsonDocument doc = JsonDocument.Parse(json);
+        if (doc.RootElement.ValueKind == JsonValueKind.Array)
+        {
+            _donationCount = doc.RootElement.GetArrayLength();
+        }
+    }
 }

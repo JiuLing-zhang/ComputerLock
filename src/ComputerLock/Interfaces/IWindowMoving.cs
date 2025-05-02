c8341cd7feb3c342492a2ce7b20e8a0e02d5604f
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2025-05-02 19:58:52 +0800

    使用组件实现自定义标题栏

--- src/ComputerLock/Interfaces/IWindowMoving.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ComputerLock.Interfaces;
-public interface IWindowMoving
-{
-    void MouseDown();
-    void MouseUp();
-}
--- src/ComputerLock/Interfaces/IWindowTitleBar.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ComputerLock.Interfaces;
-public interface IWindowTitleBar
-{
-    public bool IsMaximized { get; }
-    public void Minimize();
-    public void Maximize();
-    public void Close();
-    public void Restart();
-}
